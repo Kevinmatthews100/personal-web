fde1a9a09892a0e06facad749e6c7b6ab3116d0f
Kevin Matthews <kevinmatthews100@gmail.com>
2022-07-08 18:59:03 -0400

    chore: explicitly use adapter netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  kit: {
+    adapter: adapter(),
+  },
+
   preprocess: [
     preprocess({
       postcss: true,
