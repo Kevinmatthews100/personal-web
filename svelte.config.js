89e04d64da3949eac688dff6346ee20fe4a67ee1
Kevin Matthews <kevinmatthews100@gmail.com>
2022-09-17 20:35:22 -0400

    chore: upgrade dependencies, migrate sveltekit routes

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,27 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
-
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
   preprocess: [
     preprocess({
       postcss: true,
     }),
   ],
+
+  kit: {
+    // default options are shown
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false,
+    }),
+  },
 };
 
 export default config;
