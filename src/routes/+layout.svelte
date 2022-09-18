37e4d4b046a96d3c65f28c35f0a799e185dd2094
Kevin Matthews <kevinmatthews100@gmail.com>
2022-09-17 21:04:13 -0400

    feat: revamp site layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,21 +3,12 @@
 </script>
 
 <svelte:head>
-  <title>Kevin Matthews</title>
+  <title>Kev</title>
 </svelte:head>
 
-<header class="flex items-center my-8 mx-auto justify-evenly text-sm max-w-2xl xl:text-xl xl:mx-0 xl:justify-start">
-  <a href="/" class="text-black cursor-pointer hover:underline xl:px-5 xl:ml-40"> Kevin </a>
-  <a href="/publications" class="text-black cursor-pointer hover:underline xl:px-5"> Publications </a>
-  <a href="/music" class="text-black cursor-pointer hover:underline xl:px-5"> Music </a>
-</header>
-
-<hr class="mb-8 xl:mb-14" />
-
-<div class="flex flex-col xl:px-5 xl:flex-row xl:mt-14 xl:ml-40 xl:items-start">
-  <div class="flex flex-col">
-    <img class="w-48 mb-4 rounded-2xl mx-auto xl:w-64 xl:mx-0" src="/images/profile-pic.jpg" alt="Profile" />
-    <h1 class="mb-4 mx-auto text-3xl">Kevin Matthews</h1>
+<div class="max-w-7xl mx-auto mt-4 lg:mt-24">
+  <div class="flex flex-col items-center space-y-4 p-4">
+    <img class="w-64 rounded-3xl" src="/images/profile-pic.jpg" alt="Profile" />
     <div class="flex flex-row mx-auto mb-4 w-56">
       <a href="https://www.instagram.com/kmatthews10/" target="_blank" class="mx-auto">
         <img src="/images/insta.png" class="w-10 h-10" alt="Instagram" />
@@ -29,8 +20,14 @@
         <img src="/images/lin.png" class="w-10 h-10" alt="Linkedin" />
       </a>
     </div>
+    <div class="flex space-x-4 text-2xl">
+      <a href="/">Home</a>
+      <a href="/publications">Publications</a>
+      <a href="/music">Music</a>
+    </div>
   </div>
-  <main class="flex flex-col w-full mx-auto max-w-2xl px-2 my-4 xl:mt-0 xl:ml-24 2xl:max-w-4xl">
+
+  <main class="p-4 mx-auto">
     <slot />
   </main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<h2 class="text-3xl xl:mx-0 xl:text-4xl">Developer</h2>
-<p class="max-w-2xl mt-2 mb-10 text-md">
-  Full-stack developer at
-  <a href="https://www.labelinteractive.com/" target="_blank" rel="noreferrer">Label Interactive</a>, primarily working
-  on an e-commerce platform for a large New England retail chain, with 400,000+ monthly orders.
-</p>
-<h2 class="text-3xl xl:mx-0 xl:text-4xl">Musician</h2>
-<p class="max-w-2xl mt-2 mb-10 text-md">
-  Published composer, active contractor for original cinematic music, and research assistant at The Penn State
-  University School of Music.
-</p>
+<div class="max-w-2xl mx-auto">
+  <h2 class="text-3xl xl:mx-0 xl:text-4xl">Developer</h2>
+  <p class="max-w-2xl mt-2 mb-10 text-md">
+    Full-stack developer at
+    <a href="https://www.labelinteractive.com/" target="_blank" rel="noreferrer">Label Interactive</a>, primarily
+    working on an e-commerce platform for a large New England retail chain, with 400,000+ monthly orders.
+  </p>
+  <h2 class="text-3xl xl:mx-0 xl:text-4xl">Musician</h2>
+  <p class="max-w-2xl mt-2 mb-10 text-md">
+    Published composer, active contractor for original cinematic music, and research assistant at The Penn State
+    University School of Music.
+  </p>
+</div>
