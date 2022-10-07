0208cd60567c7b4168b73a17071c63c3bfce6b9b
Kevin Matthews <kevinmatthews100@gmail.com>
2022-10-07 13:21:19 -0400

    content: adjust wording on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <p class="mt-2 mb-10">
   I'm a full stack developer at <a href="https://www.labelinteractive.com" target="_blank" rel="noreferrer"
     >Label Interactive</a
-  >, primarily working on e-commerce ordering services for a large New England retail chain, processing 500,000+ monthly
-  orders. I'm also a published composer, an active contractor for original cinematic music, and a research assistant at
-  The Penn State University School of Music.
+  >, primarily working on e-commerce services for a large New England retail chain, processing 500,000+ monthly orders.
+  I'm also a published composer, an active contractor for original cinematic music, and a research assistant at The Penn
+  State University School of Music.
 </p>
