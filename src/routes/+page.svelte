8d4697065bd4eac207042d25cb074f7e023e34b0
Kevin Matthews <kevinmatthews100@gmail.com>
2022-10-08 18:56:06 -0400

    feat: convert most content to markdown

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1 class="text-4xl">Hello</h1>
-<p class="mt-2 mb-10">
-  I'm a full stack developer at <a href="https://www.labelinteractive.com" target="_blank" rel="noreferrer"
-    >Label Interactive</a
-  >, primarily working on e-commerce services for a large New England retail chain, processing 500,000+ monthly orders.
-  I'm also a published composer, an active contractor for original cinematic music, and a research assistant at The Penn
-  State University School of Music.
-</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', '.svx', '.md'],
+
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: [
     preprocess({
       postcss: true,
     }),
+
+    mdsvex({ extensions: ['.svx', '.md'] }),
   ],
 
   kit: {
